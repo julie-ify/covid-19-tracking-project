7654063d5de4ae528daf7b7b3860e66064148a34
Juliana Ifionu <julieifionu@gmail.com>
2021-10-12 12:27:44 +0100

    Display countries and their data on the UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 /* eslint-disable */
 
 const Main = () => {
-  return <h1>Goodby:</h1>;
+  const countries = useSelector((state) => state.countries);
+  const CountryWithRegion = [];
+  const cont = [
+    'Spain',
+    'Italy',
+    'Germany',
+    'United Kingdom',
+    'France',
+    'Netherlands',
+    'Belgium',
+    'Portugal',
+    'Switzerland',
+    'Denmark',
+  ];
+
+  countries &&
+    countries.map((country) => {
+      cont.forEach((element) => {
+        if (element === country.name) {
+          CountryWithRegion.push(country);
+        }
+      });
+    });
+  // console.log(CountryWithRegion);
+  return (
+    <div>
+      <div>
+        <ul>
+          {CountryWithRegion.map((country) => (
+            <li key={country.id}>{country.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
