d3902e862eb93e8b15ba2c3f11386172ccc6ce9c
Juliana Ifionu <julieifionu@gmail.com>
2021-10-12 18:13:10 +0100

    Display number of confirmed cases

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getCountry } from '../redux/country/country';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Header from './Header';
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 const CountryDetails = () => {
   const dispatch = useDispatch();
@@ -16,14 +18,24 @@ const CountryDetails = () => {
   if (!country) {
     return null;
   }
-  const { name,  } = country;
-  console.log(country);
+  const { name, today_confirmed, regions } = country;
 
   return (
     <div>
-     <Header countryName={name} />
-      <h1>{name}</h1>
-      <span>{}</span>
+      <Header countryName={name} />
+      <div>
+        <h1>{name}</h1>
+        <span>{today_confirmed}</span>
+      </div>
+      <div>
+        {regions.map((region) => (
+          <div>
+            <div>{region.name}</div>
+            <div>Confirmed cases: {region.today_confirmed}</div>
+            <FontAwesomeIcon icon={faArrowCircleRight} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
