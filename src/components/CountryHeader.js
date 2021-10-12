63bb0604cdbc3168b9043de10a995df5912f1014
Juliana Ifionu <julieifionu@gmail.com>
2021-10-12 19:32:19 +0100

    Style the CountryHeader component

--- src/components/CountryHeader.js
+++ src/components/CountryHeader.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobeEurope } from '@fortawesome/free-solid-svg-icons';
 
 const CountryHeader = (props) => {
   const { region } = props;
   const totalConfirmedCases = region.reduce(
     (initial, element) => initial + element.today_confirmed,
-    0,
+    0
   );
 
   return (
-    <div>
-      <h1>Total Confirmed Cases in Europe Today</h1>
-      <span>{totalConfirmedCases}</span>
+    <div className="countryHeader">
+      <div className="imgWrap">
+        <FontAwesomeIcon icon={faGlobeEurope} className="imgCover" />
+      </div>
+      <div className="headerTextWrap">
+        <div className="headerText">
+          <p className="bigHeading">Europe</p>
+          <span>Confirmed cases: {totalConfirmedCases}</span>
+        </div>
+      </div>
     </div>
   );
 };
