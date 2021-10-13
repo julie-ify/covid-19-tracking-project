f1161d017fdf63046d1a38dcabc5c9e1ff7296c4
Juliana Ifionu <julieifionu@gmail.com>
2021-10-13 11:29:43 +0100

    Implement searchBar in the main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import CountryHeader from './CountryHeader';
 import Header from './Header';
@@ -9,6 +9,7 @@ import SearchBar from './SearchBar';
 const Main = () => {
   let cardNumber = 0;
   const countries = useSelector((state) => state.countriesReducer.countries);
+  const [searchBar, setSearchBar] = useState('');
   const CountryWithRegion = [];
   const cont = [
     'Spain',
@@ -22,6 +23,10 @@ const Main = () => {
     'Switzerland',
     'Denmark',
   ];
+
+  const handleChange = (e) => {
+    setSearchBar(() => e.target.value);
+  };
 
   countries &&
     countries.map((country) => {
@@ -38,10 +43,22 @@ const Main = () => {
         <CountryHeader region={CountryWithRegion} />
         <div className="countrySearch">
           <div className="countries">region breakdown - 2021</div>
-          <SearchBar />
+          <SearchBar value={searchBar} handleInput={handleChange} />
         </div>
         <ul className="mainGrid">
-          {CountryWithRegion.map((country) => {
+          {CountryWithRegion.filter((item) => {
+            if (searchBar === '') {
+              return item;
+            } else {
+              if (
+                item.name
+                  .toLowerCase()
+                  .includes(searchBar.toLocaleLowerCase())
+              ) {
+                return item;
+              }
+            }
+          }).map((country) => {
             if (cardNumber + 1 > 4) {
               cardNumber = 1;
             } else {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const SearchBar = ({ searchValue, handleChange }) => (
-  <input placeholder="search country" className="searchBar" value={searchValue} onChange={handleChange} />
+const SearchBar = ({ searchValue, handleChange, handleInput }) => (
+  <input placeholder="search country" className="searchBar" value={searchValue} onChange={handleChange ? handleChange : handleInput} />
 );
 
 export default SearchBar;
