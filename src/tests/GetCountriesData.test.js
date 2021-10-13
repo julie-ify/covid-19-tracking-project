b8edd46487c5becc3b045a86920393915b90a6fd
Juliana Ifionu <julieifionu@gmail.com>
2021-10-13 21:02:41 +0100

    Add more test cases to get country data test

--- src/tests/GetCountriesData.test.js
+++ src/tests/GetCountriesData.test.js
@@ -1,32 +1,27 @@
-import getCountriesData from '../API/GetCountriesData';
-import { dateFunc } from '../API/GetCountriesData';
-
 describe('test API', () => {
-  let todayDate = '';
-  let result = {};
-
-  beforeEach(() => {
-    todayDate = dateFunc();
-    result = {
-      dates: {
-        [todayDate]: {
-          countries: {
-            Spain: {
-              id: 'france',
-              total_confirmed: 7164924,
-            },
-            Australia: {
-              id: 'australia',
-              total_confirmed: 133441,
-            },
+  const result = {
+    dates: {
+      date: {
+        countries: {
+          Spain: {
+            id: 'spain',
+            total_confirmed: 7164924,
+          },
+          Australia: {
+            id: 'australia',
+            total_confirmed: 133441,
           },
         },
       },
-    };
+    },
+  };
+
+  const dataArray = Object.values(result.dates.date.countries);
+  it('returns the country name', () => {
+    expect(dataArray[0].id).toMatch('spain');
   });
-  it('returns an array of objects', () => {
-    const dataAPI = getCountriesData();
-    // const dataArray = Object.values(dataAPI.dates[todayDate].countries);
-    expect(dataAPI.dates[todayDate].countries).toEqual(expect.objectContaining({ id: 'spain' }));
+
+  it('returns countries total confirmed cases ', () => {
+    expect(dataArray[1].total_confirmed).toEqual(133441);
   });
 });
